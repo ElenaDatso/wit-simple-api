418a8f7163a051cdaf87f9d49c6b8081201be9c9
Olena Datso <elena.shcheglova@gmail.com>
2025-02-14 23:28:22 -0700

    refactor: add if exists check

--- middleware/deleteHandler.js
+++ middleware/deleteHandler.js
@@ -1,10 +1,13 @@
-
-const { ref, remove } = require('firebase/database');
+const { ref, get, remove, child } = require('firebase/database');
 const { db } = require('../config/init.js');
 
 // Delete data from the database
 module.exports = async (req, res, next) => {
   try {
+    const snapshot = await get(child(ref(db), `${req.path}`));
+    if (!snapshot.exists()) {
+      return res.status(404).send('Data not found');
+    }
     await remove(ref(db, `${req.path}`));
     res.send('Data deleted successfully');
   } catch (error) {
--- middleware/putHandler.js
+++ middleware/putHandler.js
@@ -4,10 +4,14 @@ const { db } = require('../config/init.js');
 // Update data in the database
 module.exports = async (req, res, next) => {
   try {
+    const snapshot = await get(child(ref(db), `${req.path}`));
+    if (!snapshot.exists()) {
+      return res.status(404).send('Data not found');
+    }
     await update(ref(db, `${req.path}`), { ...req.body });
     res.send('Data updated successfully');
   } catch (error) {
     res.status(error.status || 500).send(error.message || 'Unknown error');
-  };
+  }
   next();
 };
