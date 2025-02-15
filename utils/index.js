04e12f8f4508f2d4fc06d3651c9e6fc79a25dbdb
Olena Datso <elena.shcheglova@gmail.com>
2025-02-14 20:03:08 -0700

    feat: add data validation

---
+++ utils/index.js
@@ -0,0 +1,8 @@
+const { newDataValidation, updateDataValidation } = require('./validation.js');
+const commonErrorHandler = require('./errorHandler.js');
+
+module.exports = {
+  newDataValidation,
+  updateDataValidation,
+  commonErrorHandler,
+};
--- utils/validation.js
+++ utils/validation.js
@@ -1,26 +1,36 @@
-const newUserDataValidation = ({
-  name,
-  username,
-  email,
-  address = {
-    street: 'Optional',
-    suite: 'Optional',
-    city: 'Optional',
-    zipcode: 'Optional',
-  },
-}) => {
-  if (
-    !name ||
-    !username ||
-    !email ||
-    !address?.street ||
-    !address?.suite ||
-    !address?.city ||
-    !address?.zipcode
-  ) {
-    return 'Please provide all required fields';
+const newDataValidation = (data, schema) => {
+  for (const key in data) {
+    if (typeof data[key] === 'object') {
+      newDataValidation(data[key], schema[key]);
+    }
+    if (
+      !schema.hasOwnProperty(key) ||
+      data[key] === null ||
+      data[key] === undefined ||
+      data[key] === ''
+    ) {
+      if (key === 'id') {
+        throw new Error(`The field ${key} is not allowed and set automatically`);
+      }
+      throw new Error(
+        `Please provide all required fields, the failed field is ${key}`
+      );
+    }
   }
-  return { name, username, email, address };
 };
 
-module.exports = { newUserDataValidation };
+const updateDataValidation = (data, schema) => {
+  for (const key in data) {
+    if (typeof data[key] === 'object') {
+      updateDataValidation(data[key], schema[key]);
+    }
+    if (!schema.hasOwnProperty(key)) {
+      if (key === 'id') {
+        throw new Error(`The field ${key} is set automatically`);
+      }
+      throw new Error(`The field ${key} is not allowed in the schema`);
+    }
+  }
+};
+
+module.exports = { newDataValidation, updateDataValidation };
